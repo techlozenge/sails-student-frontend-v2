9aed81647454008009fdd36af705bd6f1fb34778
Daniel Ford <daniel.ford@libertyMutual.com>
2019-07-30 14:25:09 -0400

    added doc for backend and frontend

--- assets/js/course_admin_utils.js
+++ assets/js/course_admin_utils.js
@@ -1,6 +1,6 @@
 (function() {
     $(function() {
-        $('#navbar-test').on('click', "#srchButton", function(e) {
+        $('#navbar').on('click', "#srchButton", function(e) {
             window.open("http://www.google.com/search?q=" + ($("#srchText").val()).split(' ').join('+'), "Search");
         });
     });
